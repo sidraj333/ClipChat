df4dcf09214482711abf164a1411ea0b636bbba7
Siddharth Rajagopalan <sraja21@illinois.edu>
2025-10-02 23:33:07 -0500

    created sidepanel for each tab independent

--- public/background.js
+++ public/background.js
@@ -14,20 +14,19 @@ chrome.runtime.onInstalled.addListener(() => {
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === 'openSidePanel') {
     // This will open the panel in all the pages on the current window.
-    chrome.sidePanel.open({ windowId: tab.id });
+    chrome.sidePanel.open({ tabId: tab.id });
   }
 });
-
-
-
-chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
-  // if (!tab.url) return;
+//listener to handle user switching between tabs
+chrome.tabs.onActivated.addListener(async ({ tabId }) => {
+  const tab = await chrome.tabs.get(tabId);
+  if (tab.url === '') {return;}
+  console.log(tab)
   const url = new URL(tab.url);
-  // Enables the side panel on youtube.com
   if (YOUTUBE_ORIGINS.has(url.origin) && url.pathname === YOUTUBE_PATHNAME) {
     console.log("user has entered youtube video: ", tab.title)
     await chrome.sidePanel.setOptions({
-      tabId,
+      tabId: tab.id,
       path: './index.html',
       enabled: true
     });
@@ -35,7 +34,33 @@ chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
     console.log("user exited youtube video")
     // Disables the side panel on all other sites
     await chrome.sidePanel.setOptions({
-      tabId,
+      tabId: tab.id,
+      enabled: false
+    });
+  }
+});
+
+
+
+
+//listener to handle user changing url in current tab
+chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
+  // if (!tab.url) return;
+  const url = new URL(tab.url);
+  // Enables the side panel on youtube.com
+  if (tab.url === '') {return;}
+  if (YOUTUBE_ORIGINS.has(url.origin) && url.pathname === YOUTUBE_PATHNAME) {
+    console.log("user has entered youtube video: ", tab.title)
+    await chrome.sidePanel.setOptions({
+      tabId: tab.id,
+      path: './index.html',
+      enabled: true
+    });
+  } else {
+    console.log("user exited youtube video")
+    // Disables the side panel on all other sites
+    await chrome.sidePanel.setOptions({
+      tabId: tab.id,
       enabled: false
     });
   }
