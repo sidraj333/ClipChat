fb6630da804f225a97548391ebc3fcfa61e66e37
Siddharth Rajagopalan <sraja21@illinois.edu>
2025-10-03 22:57:10 -0500

    created helper functions for clear consise code

--- public/background.js
+++ public/background.js
@@ -11,33 +11,19 @@ chrome.runtime.onInstalled.addListener(() => {
   });
 });
 
+
+//enables user to enable sidepanel using toolbar icon
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === 'openSidePanel') {
-    // This will open the panel in all the pages on the current window.
+    // This will open the panel in all the pages on the current tab.
     chrome.sidePanel.open({ tabId: tab.id });
   }
 });
+
 //listener to handle user switching between tabs
 chrome.tabs.onActivated.addListener(async ({ tabId }) => {
   const tab = await chrome.tabs.get(tabId);
-  if (tab.url === '') {return;}
-  console.log(tab)
-  const url = new URL(tab.url);
-  if (YOUTUBE_ORIGINS.has(url.origin) && url.pathname === YOUTUBE_PATHNAME) {
-    console.log("user has entered youtube video: ", tab.title)
-    await chrome.sidePanel.setOptions({
-      tabId: tab.id,
-      path: './index.html',
-      enabled: true
-    });
-  } else {
-    console.log("user exited youtube video")
-    // Disables the side panel on all other sites
-    await chrome.sidePanel.setOptions({
-      tabId: tab.id,
-      enabled: false
-    });
-  }
+  enableSidePanel(tab)
 });
 
 
@@ -45,10 +31,15 @@ chrome.tabs.onActivated.addListener(async ({ tabId }) => {
 
 //listener to handle user changing url in current tab
 chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
-  // if (!tab.url) return;
+  enableSidePanel(tab);
+});
+
+
+
+//Helpers
+async function enableSidePanel(tab) {
+  if (!tab.url) return;
   const url = new URL(tab.url);
-  // Enables the side panel on youtube.com
-  if (tab.url === '') {return;}
   if (YOUTUBE_ORIGINS.has(url.origin) && url.pathname === YOUTUBE_PATHNAME) {
     console.log("user has entered youtube video: ", tab.title)
     await chrome.sidePanel.setOptions({
@@ -64,6 +55,6 @@ chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
       enabled: false
     });
   }
-});
 
+}
 
